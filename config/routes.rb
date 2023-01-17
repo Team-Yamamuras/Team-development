53a2610d77b96bdd476b60e9227f9cf6316bc19b
poyonosk <117244700+poyonosk@users.noreply.github.com>
2023-01-17 04:42:59 +0000

    [fix]migrationfile

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
-  devise_for :users		
 		
   root "homes#top"		
   get "/home/about" => "homes#about", as: "about"		
