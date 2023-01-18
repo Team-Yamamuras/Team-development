004b0cc10f98f8feecfecf965d0a1c2cfbec8b31
youyou1605 <64137553+youyou1605@users.noreply.github.com>
2023-01-18 11:28:09 +0000

    [Add] items

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
+      @categories = Category.where(is_enabled: true)
+    if params[category_id]
+      @category = Category.find(params[:category_id])
+      @items = @category.items.order(created_at: :desc).where(is_active: true).page(params[:page]).per(8)
+    else
+      @items = Item.where(is_active: true).page(params[:page]).per(12)
+    end
   end
 
   def show
+    @cart_item = CartItem.new
+    @categories = Category.all
+    @item = Item.find(params[:id])
   end
 end
