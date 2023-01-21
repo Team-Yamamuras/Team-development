3bad6369dbd5a5b3de29ec78dc64f8e872a2092b
kyousuke678 <kyousuke412678@gmail.com>
2023-01-21 06:34:17 +0000

    top about画面作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
+  before_action :authenticate_customer!, except: [:top, :about]
+
   def top
+    @items = Item.where(is_active: true)
   end
 
   def about
