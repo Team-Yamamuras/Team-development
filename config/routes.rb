7e4552b2df67503901387e47b62498195725e75b
EC2 Default User <ec2-user@ip-172-31-47-102.ap-northeast-1.compute.internal>
2023-01-22 10:46:19 +0000

    Add admin customer

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ Rails.application.routes.draw do
     root 'homes#top'
     resources :items,only: [:new,:index,:show,:edit,:update]
     resources :categories,only: [:index,:create,:edit,:update]
+    resources :customers,only: [:index,:show,:edit,:update]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
