f02787ac3b990978d9e02d00236b400f64d5e48d
EC2 Default User <ec2-user@ip-172-31-47-102.ap-northeast-1.compute.internal>
2023-01-23 12:05:57 +0000

    Ass admin orders controller

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,23 @@
+class Admin::OrdersController < ApplicationController
+  
+  def index
+    @order = Orders.all
+    @order = Orders.page(params[:page]).per(10)
+  end
+  
+  def show
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_orders_path
+  end
+  
+ private
+ 
+  def order_params
+    params.require(:order).permit(:postage, :billing, :payment, :name, :zip_code, :address, :status)
+  end
+ 
+end
