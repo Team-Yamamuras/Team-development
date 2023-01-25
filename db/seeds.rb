6acd17921a4e558e3d98cb78c0322b0b282a032d
poyonosk <117244700+poyonosk@users.noreply.github.com>
2023-01-25 01:40:30 +0000

    [FIX]code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(email: "yamamura@gmail.com", password: "sadako")
+
+Category.create(category: "test")
+
+10.times do |n|
+    Item.create(
+        category_id: 1,
+        name: "アイスクリーム#{n}",
+        introduction: "テスト",
+        price_without_tax: 200,
+        is_active: true,
+        image: ActiveStorage::Blob.create_and_upload!(io: File.open(File.join(Rails.root,"app/assets/images/no_image.jpg")), filename:"no_image.jpg")
+    )
+end
